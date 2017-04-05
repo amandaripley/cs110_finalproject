f9d897939c33658a6de233d9133f373f65ac3d8a
amandaripley <aripley1@binghamton.edu>
2017-04-05 18:19:18 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,20 +1,20 @@
-import Number 
+import class_number 
 import keyclass
-import remotelaptopimageclass
+#import remotelaptopimageclass
 
 def main():
-	print("=====Test Number Model=====")
-    	test_number = number.Number(20,30)
-	print(test_number)
+    print("=====Test Number Model=====")
+    test_number = class_number.Number(20,30,7)
+    print(test_number)
     
-    	print("====Test Sample Input===")
-    	coordinates = test_number.move(50,50)
-    	print("The new coordinates are:", coordinates)
+    print("====Test Sample Input===")
+    coordinates = test_number.move(50,50)
+    print("The new coordinates are:", coordinates)
 		
-	print("====Test Key Model====")
-	key = keyclass.Key("Key", "Triangle", 20,30):
-	key.open("Triangle")
-	key.open("Circle")
+    print("====Test Key Model====")
+    key = keyclass.Key("Key", "Triangle", 20,30)
+    key.openbox("Triangle")
+    key.openbox("Circle")
 
 	
 
